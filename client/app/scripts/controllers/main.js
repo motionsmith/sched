03480194d34a13708908174204cc8839d968fad4
Eric Smith <motionsmith@gmail.com>
2015-10-20 19:24:10 -0700

    _

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -21,6 +21,22 @@ angular.module('schedApp')
       parseFunctions.cancelAppointment(appt.objectId).then(refreshData);
     };
 
+    $scope.seeMore = function (avails) {
+      if (avails === $scope.availsAsap) {
+        $scope.asapLimit = $scope.availsAsap.length + 6;
+        $scope.availsAsap = availabilityHelper.filterSoonest($scope.asapLimit);
+      } else if (avails === $scope.availsMornings) {
+        $scope.morningsLimit = $scope.availsMornings.length + 6;
+        $scope.availsMornings = availabilityHelper.filterMornings($scope.morningsLimit);
+      } else if (avails === $scope.availsAfternoons) {
+        $scope.afternoonsLimit = $scope.availsAfternoons.length + 6;
+        $scope.availsAfternoons = availabilityHelper.filterAfternoons($scope.afternoonsLimit);
+      } else if (avails === $scope.availsLater) {
+        $scope.laterLimit = $scope.availsLater.length + 6;
+        $scope.availsLater = availabilityHelper.filterLater($scope.laterLimit);
+      }
+    };
+
     function refreshData() {
       $scope.refreshingData = true;
       var refreshPromises = [];
@@ -44,7 +60,7 @@ angular.module('schedApp')
 
     function processAvailabilities() {
       availabilityHelper.initialize(coachAvailability.data.result);
-      $scope.availsAsap = availabilityHelper.filterSoonest(3);
+      $scope.availsAsap = availabilityHelper.filterSoonest($scope.asapLimit);
       $scope.availsMornings = availabilityHelper.filterMornings(3);
       $scope.availsAfternoons = availabilityHelper.filterAfternoons(3);
       $scope.availsLater = availabilityHelper.filterLater(3);
@@ -56,8 +72,12 @@ angular.module('schedApp')
 
     var moment = $window.moment;
 
+    $scope.coachName = myCoach.data.firstName + ' ' + myCoach.data.lastName;
+    $scope.asapLimit = 3;
+    $scope.morningsLimit = 3;
+    $scope.afternoonsLimit = 3;
+    $scope.laterLimit = 3;
+
     processAvailabilities();
     processAppointments();
-
-    $scope.coachName = myCoach.data.firstName + ' ' + myCoach.data.lastName;
   });
